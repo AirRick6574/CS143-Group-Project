d670034c2407ecefd4dca832fc741f05923da805
98f6gb6dw7-sketch <98f6gb6dw7@privaterelay.appleid.com>
2025-11-21 13:44:55 -0800

    Just organizing thoughts to start

---
+++ src/scoreboard.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+
+/**
+ * Class is meant to track pairs each player has, rank the players, return the players
+ * in ranked order, and return the winner who has the most pairs.
+ */
+public class scoreboard {
+	
+	// use totalSetsCount from the player class as the variable
+	
+	// create method that takes totalSetsCount from each player and creates a queue.
+	
+	// what is the most efficient way to rearrange the queue/stack/or array list?
+	
+	// end result is to print rankings highest to lowest and return the highest as winner
+	
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
